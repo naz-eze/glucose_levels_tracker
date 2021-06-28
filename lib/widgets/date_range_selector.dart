bf18c4a948e90512ff7b8970002be994e473b85e
Naz <naz_eze@yahoo.co.uk>
2021-06-28 07:55:56 +0200

    Adds functionality for selecting a start and end date range

--- lib/widgets/date_range_selector.dart
+++ lib/widgets/date_range_selector.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:glucose_levels_tracker/extensions/date_formatter.dart';
 
 class DateRangeSelector extends StatelessWidget {
-  const DateRangeSelector({Key? key}) : super(key: key);
+  final DateTime startDate, endDate;
+  final DateTimeRange selectedRange;
+
+  const DateRangeSelector(
+      {Key? key,
+      required this.startDate,
+      required this.endDate,
+      required this.selectedRange})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    Future _selectDateRange(BuildContext context) async {
+      final newDateRange = await showDateRangePicker(
+        context: context,
+        firstDate: startDate,
+        lastDate: endDate,
+        initialDateRange: selectedRange,
+      );
+      if (newDateRange == null) return;
+    }
+
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            ElevatedButton(
+              child: Text(
+                startDate.dmy(),
+                style: TextStyle(fontSize: 18, color: Colors.black),
+              ),
+              onPressed: () => _selectDateRange(context),
+            ),
+            ElevatedButton(
+              child: Text(
+                endDate.dmy(),
+                style: TextStyle(fontSize: 18, color: Colors.black),
+              ),
+              onPressed: () => _selectDateRange(context),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- test/widgets/date_range_selector_test.dart
+++ test/widgets/date_range_selector_test.dart
@@ -4,10 +4,57 @@ import 'package:glucose_levels_tracker/widgets/date_range_selector.dart';
 
 void main() {
   testWidgets('Date Range Selector test', (WidgetTester tester) async {
-    await tester.pumpWidget(DateRangeSelector());
+    final DateTime startDate = DateTime(2021, 06, 28);
+    final DateTime endDate = DateTime(2021, 07, 12);
+    final DateTimeRange selectedRange =
+        DateTimeRange(start: startDate, end: endDate);
 
-    final container =
-        find.byType(Container).evaluate().single.widget as Container;
-    expect(container.child, null);
+    // Build DateRangeSelector widget
+
+    await tester.pumpWidget(MaterialApp(
+      home: DateRangeSelector(
+        startDate: startDate,
+        endDate: endDate,
+        selectedRange: selectedRange,
+      ),
+    ));
+
+    // Verify Table widget exist and contains one child
+    final table = find.byType(Table).evaluate().single.widget as Table;
+    expect(table.children.length, 1);
+
+    // Verify TableRow contains two children
+    final tableRow = table.children[0];
+    expect(tableRow.children, isNot(null));
+    expect(tableRow.children?.length, 2);
+
+    // Verify details on first table row
+    final startDateButton = tableRow.children?.first as ElevatedButton;
+    final startDateButtonText = startDateButton.child as Text;
+    expect(startDateButtonText.data, '28/06/2021');
+    expect(
+      startDateButtonText.style,
+      TextStyle(fontSize: 18, color: Colors.black),
+    );
+    await verifyDatePickerDialogOpens(tester, '28/06/2021');
+
+    // Verify details on second table row
+    final endDateButton = tableRow.children?.last as ElevatedButton;
+    final endDateButtonText = endDateButton.child as Text;
+    expect(endDateButtonText.data, '12/07/2021');
+    expect(
+      endDateButtonText.style,
+      TextStyle(fontSize: 18, color: Colors.black),
+    );
+    await verifyDatePickerDialogOpens(tester, '12/07/2021');
   });
+}
+
+Future<void> verifyDatePickerDialogOpens(
+    WidgetTester tester, String text) async {
+  await tester.tap(find.text(text));
+  await tester.pump();
+  expect(find.byType(DateRangePickerDialog), findsOneWidget);
+  await tester.tap(find.byIcon(Icons.close));
+  await tester.pumpAndSettle();
 }
