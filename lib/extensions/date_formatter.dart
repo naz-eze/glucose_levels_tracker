6137ffcf7395e4b9ea991d83e7407537799b3835
Naz <naz_eze@yahoo.co.uk>
2021-06-28 15:09:29 +0200

    Adds extension function to support DD/MM format

--- lib/extensions/date_formatter.dart
+++ lib/extensions/date_formatter.dart
@@ -1,5 +1,9 @@
 extension DateFormatter on DateTime {
   String dmy() {
-    return "${this.day.toString().padLeft(2, '0')}/${this.month.toString().padLeft(2, '0')}/${this.year.toString()}";
+    return "${dm()}/${this.year.toString()}";
+  }
+
+  String dm() {
+    return "${this.day.toString().padLeft(2, '0')}/${this.month.toString().padLeft(2, '0')}";
   }
 }
--- test/extensions/date_formatter_test.dart
+++ test/extensions/date_formatter_test.dart
@@ -7,5 +7,10 @@ void main() {
       final DateTime dateTime = DateTime(2021, 06, 28);
       expect(dateTime.dmy(), '28/06/2021');
     });
+
+    test('dm function should format date in DD/MM format', () {
+      final DateTime dateTime = DateTime(2021, 11, 30);
+      expect(dateTime.dm(), '30/11');
+    });
   });
 }
