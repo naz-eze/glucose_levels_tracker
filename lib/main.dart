98de4e43d0d06410c0eb44ae290c978acfdb3778
Naz <naz_eze@yahoo.co.uk>
2021-06-29 09:29:44 +0200

    Updates Main widget to build Home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:glucose_levels_tracker/models/glucose_measurment.dart';
+import 'package:glucose_levels_tracker/notifiers/glucose_measurement_notifier.dart';
+import 'package:glucose_levels_tracker/services/glucose_service.dart';
+import 'package:glucose_levels_tracker/widgets/home.dart';
+import 'package:http/http.dart' show Client;
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(Main());
+  final glucoseService = GlucoseService(
+      'https://s3-de-central.profitbricks.com/una-health-frontend-tech-challenge/sample.json',
+      Client());
+  runApp(Main(glucoseService));
 }
 
 class Main extends StatelessWidget {
   // This widget is the root of your application.
   final appTitle = 'Glucose Levels Tracker';
+  final GlucoseService glucoseService;
+  const Main(this.glucoseService);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,28 +26,30 @@ class Main extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(title: appTitle),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: FutureBuilder(
+          future: glucoseService.fetchMeasurements(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final measurements = snapshot.data as List<GlucoseMeasurement>;
+              return SingleChildScrollView(
+                key: Key('main_home_key'),
+                child: ChangeNotifierProvider(
+                  create: (context) => GlucoseMeasurementNotifier(measurements),
+                  child: Home(),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('An error occurred fetching glucose measurments');
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
-      body: Container(),
     );
   }
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:glucose_levels_tracker/main.dart';
+import 'package:glucose_levels_tracker/models/glucose_measurment.dart';
+import 'package:glucose_levels_tracker/services/glucose_service.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
 
+import 'main_test.mocks.dart';
+
+@GenerateMocks([GlucoseService])
 void main() {
-  testWidgets('Main test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(Main());
-    var title = 'Glucose Levels Tracker';
+  late GlucoseService glucoseService;
 
-    // Verify MaterialApp properties
-    final app =
-        find.byType(MaterialApp).evaluate().single.widget as MaterialApp;
-    expect(app.title, title);
-    expect(ThemeData(primarySwatch: Colors.blue), app.theme);
+  setUp(() {
+    glucoseService = MockGlucoseService();
+  });
+  group('Main Test', () {
+    List<GlucoseMeasurement> measurements = [
+      GlucoseMeasurement(6.7, DateTime(2021, 04, 27), 'mmol/L')
+    ];
 
-    // Verify AppBar widget title
-    expect(find.text(title), findsOneWidget);
+    testWidgets('should app title', (WidgetTester tester) async {
+      when(glucoseService.fetchMeasurements())
+          .thenAnswer((_) => Future.value(measurements));
+
+      await tester.pumpWidget(Main(glucoseService));
+      var title = 'Glucose Levels Tracker';
+
+      final app =
+          find.byType(MaterialApp).evaluate().single.widget as MaterialApp;
+      expect(app.title, title);
+      expect(ThemeData(primarySwatch: Colors.blue), app.theme);
+
+      // Verify AppBar widget title
+      expect(find.text(title), findsOneWidget);
+    });
+
+    testWidgets('should show a progress indicator when loading',
+        (WidgetTester tester) async {
+      when(glucoseService.fetchMeasurements()).thenAnswer(
+          (_) => Future.delayed(Duration(seconds: 1), () => measurements));
+
+      await tester.pumpWidget(Main(glucoseService));
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      await tester.pumpAndSettle();
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+    });
+
+    testWidgets('should show error message when error occurs',
+        (WidgetTester tester) async {
+      when(glucoseService.fetchMeasurements())
+          .thenAnswer((_) => Future.error('An error occurred'));
+
+      await tester.pumpWidget(Main(glucoseService));
+      await tester.pumpAndSettle();
+      expect(find.text('An error occurred fetching glucose measurments'),
+          findsOneWidget);
+    });
+
+    testWidgets('should build home widget', (WidgetTester tester) async {
+      when(glucoseService.fetchMeasurements())
+          .thenAnswer((_) => Future.value(measurements));
+
+      await tester.pumpWidget(Main(glucoseService));
+      await tester.pumpAndSettle();
+      expect(find.byKey(Key('main_home_key')), findsOneWidget);
+
+      final homeView = find.byKey(Key('main_home_key')).evaluate().single.widget
+          as SingleChildScrollView;
+
+      expect(homeView.child, isInstanceOf<ChangeNotifierProvider>());
+      expect(find.byKey(Key('home_column_key')), findsOneWidget);
+    });
   });
 }
