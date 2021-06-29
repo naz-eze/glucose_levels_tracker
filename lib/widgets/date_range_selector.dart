f8dab8279c188b22ae94305cadf2b084d0129f22
Naz <naz_eze@yahoo.co.uk>
2021-06-29 07:16:22 +0200

    Adds callback function when date range is selected and some padding

--- lib/widgets/date_range_selector.dart
+++ lib/widgets/date_range_selector.dart
@@ -4,13 +4,16 @@ import 'package:glucose_levels_tracker/extensions/date_formatter.dart';
 class DateRangeSelector extends StatelessWidget {
   final DateTime startDate, endDate;
   final DateTimeRange selectedRange;
+  final Function(DateTimeRange newDateRange) onSelected;
+  final defaultPadding = const EdgeInsets.all(8.0);
 
-  const DateRangeSelector(
-      {Key? key,
-      required this.startDate,
-      required this.endDate,
-      required this.selectedRange})
-      : super(key: key);
+  const DateRangeSelector({
+    Key? key,
+    required this.startDate,
+    required this.endDate,
+    required this.selectedRange,
+    required this.onSelected,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,21 +23,29 @@ class DateRangeSelector extends StatelessWidget {
         firstDate: startDate,
         lastDate: endDate,
         initialDateRange: selectedRange,
+        saveText: 'DONE',
       );
       if (newDateRange == null) return;
+      onSelected(newDateRange);
     }
 
     return Table(
       children: [
         TableRow(
           children: [
-            DateSelector(
-              text: startDate.dmy(),
-              onPressed: () => _selectDateRange(context),
+            Padding(
+              padding: defaultPadding,
+              child: DateSelector(
+                text: selectedRange.start.dmy(),
+                onPressed: () => _selectDateRange(context),
+              ),
             ),
-            DateSelector(
-              text: endDate.dmy(),
-              onPressed: () => _selectDateRange(context),
+            Padding(
+              padding: defaultPadding,
+              child: DateSelector(
+                text: selectedRange.end.dmy(),
+                onPressed: () => _selectDateRange(context),
+              ),
             ),
           ],
         ),
@@ -55,10 +66,7 @@ class DateSelector extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ElevatedButton(
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 18, color: Colors.black),
-        ),
+        child: Text(text, style: TextStyle(fontSize: 18)),
         onPressed: onPressed,
       );
 }
--- test/widgets/date_range_selector_test.dart
+++ test/widgets/date_range_selector_test.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:glucose_levels_tracker/widgets/date_range_selector.dart';
+import 'package:mockito/mockito.dart';
+
+class _MockCallbackFn extends Mock {
+  call(DateTimeRange dateTimeRange);
+}
 
 void main() {
-  testWidgets('Date Range Selector test', (WidgetTester tester) async {
-    final DateTime startDate = DateTime(2021, 06, 28);
-    final DateTime endDate = DateTime(2021, 07, 12);
-    final DateTimeRange selectedRange =
-        DateTimeRange(start: startDate, end: endDate);
+  late DateTime startDate;
+  late DateTime endDate;
+  late DateTimeRange selectedRange;
+  late _MockCallbackFn onSelectedFn;
 
+  setUp(() {
+    startDate = DateTime(2021, 06, 28);
+    endDate = DateTime(2021, 07, 12);
+    selectedRange = DateTimeRange(start: startDate, end: endDate);
+    onSelectedFn = _MockCallbackFn();
+  });
+
+  tearDown(() {
+    reset(onSelectedFn);
+  });
+
+  testWidgets('Date Range Selector test', (WidgetTester tester) async {
     // Build DateRangeSelector widget
     await tester.pumpWidget(MaterialApp(
       home: DateRangeSelector(
         startDate: startDate,
         endDate: endDate,
         selectedRange: selectedRange,
+        onSelected: onSelectedFn,
       ),
     ));
 
@@ -28,14 +45,42 @@ void main() {
     expect(tableRow.children?.length, 2);
 
     // Verify details on first table row
-    final startDateSelector = tableRow.children?.first as DateSelector;
+    final startDateSelectorPadding = tableRow.children?.first as Padding;
+    expect(startDateSelectorPadding.padding, EdgeInsets.all(8.0));
+
+    final startDateSelector = startDateSelectorPadding.child as DateSelector;
     expect(startDateSelector.text, '28/06/2021');
-    await verifyDatePickerDialogOpens(tester, '28/06/2021');
+    await verifyDatePickerDialogOpensAndSaves(
+      tester,
+      '28/06/2021',
+      onSelectedFn,
+      selectedRange,
+    );
+    await verifyDatePickerDialogOpensAndCloses(
+      tester,
+      '28/06/2021',
+      onSelectedFn,
+      selectedRange,
+    );
 
     // Verify details on second table row
-    final endDateButton = tableRow.children?.last as DateSelector;
-    expect(endDateButton.text, '12/07/2021');
-    await verifyDatePickerDialogOpens(tester, '12/07/2021');
+    final endDateSelectorPadding = tableRow.children?.last as Padding;
+    expect(endDateSelectorPadding.padding, EdgeInsets.all(8.0));
+
+    final endDateSelector = endDateSelectorPadding.child as DateSelector;
+    expect(endDateSelector.text, '12/07/2021');
+    await verifyDatePickerDialogOpensAndSaves(
+      tester,
+      '12/07/2021',
+      onSelectedFn,
+      selectedRange,
+    );
+    await verifyDatePickerDialogOpensAndCloses(
+      tester,
+      '12/07/2021',
+      onSelectedFn,
+      selectedRange,
+    );
   });
 
   testWidgets('Date Selector test', (WidgetTester tester) async {
@@ -51,16 +96,42 @@ void main() {
     final buttonText = button.child as Text;
 
     expect(buttonText.data, '29/06/2021');
-    expect(buttonText.style, TextStyle(fontSize: 18, color: Colors.black));
+    expect(buttonText.style, TextStyle(fontSize: 18));
     expect(button.onPressed, onPressedFn);
   });
 }
 
-Future<void> verifyDatePickerDialogOpens(
-    WidgetTester tester, String text) async {
-  await tester.tap(find.text(text));
-  await tester.pump();
+Future<void> verifyDatePickerDialogOpensAndSaves(
+  WidgetTester tester,
+  String text,
+  _MockCallbackFn onSelectedFn,
+  DateTimeRange selectedRange,
+) async {
+  await openPickerDialog(tester, text);
   expect(find.byType(DateRangePickerDialog), findsOneWidget);
+
+  expect(find.text('DONE'), findsOneWidget);
+  await tester.tap(find.text('DONE'));
+  await tester.pumpAndSettle();
+
+  verify(onSelectedFn(selectedRange)).called(1);
+}
+
+Future<void> verifyDatePickerDialogOpensAndCloses(
+    WidgetTester tester,
+    String text,
+    _MockCallbackFn onSelectedFn,
+    DateTimeRange selectedRange) async {
+  await openPickerDialog(tester, text);
+  expect(find.byType(DateRangePickerDialog), findsOneWidget);
+
   await tester.tap(find.byIcon(Icons.close));
   await tester.pumpAndSettle();
+
+  verifyNever(onSelectedFn(selectedRange));
+}
+
+Future<void> openPickerDialog(WidgetTester tester, String text) async {
+  await tester.tap(find.text(text));
+  await tester.pump();
 }
